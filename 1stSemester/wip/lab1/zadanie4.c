4dff378a69edea00fcd0f61271a59608b4b529d5
michalwrpo <michal.p.walus@gmail.com>
2023-10-26 00:39:29 +0200

    code optimalization

--- 1stSemester/wip/lab1/zadanie4.c
+++ 1stSemester/wip/lab1/zadanie4.c
@@ -13,9 +13,6 @@ int main(void) {
         for(int j = 0; j < 2 * i + 1; j++) {
             printf("*");
         }
-        for(int j = 0; j < n-1-i; j++) {
-            printf(" ");
-        }
         printf("\n");
     }
     return 0;
