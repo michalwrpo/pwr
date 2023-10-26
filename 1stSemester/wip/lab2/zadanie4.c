160d8eb9dcfb5e28c10438da73a91a7d36d806a4
michalwrpo <michal.p.walus@gmail.com>
2023-10-26 22:46:53 +0200

    Finished ex 4

--- 1stSemester/wip/lab2/zadanie4.c
+++ 1stSemester/wip/lab2/zadanie4.c
@@ -1,11 +1,11 @@
 // Zadanie 4, zadanie4.c Michał Waluś
 
 #include <stdio.h>
+#include <time.h>
 
 int daysTillDate(int day, int month, int year) {
     int przestepne, days = day - 1;
     przestepne = (year / 4) - (year / 100) + (year / 400);
-    printf("%d\n", przestepne);
     switch (month) {
     case 2:
         days += 31;
@@ -45,10 +45,14 @@ int daysTillDate(int day, int month, int year) {
     if( ( (year % 4 == 0 && (year % 100 != 0)) || year % 400 == 0 ) && month < 3 ) {
         days -= 1;
     }
-    printf("%d\n", days);
     return days;
 }
 
 int main(void) {
-    daysTillDate(1, 3, 96);
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    int days;
+    days = daysTillDate(tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900) - daysTillDate(05, 07, 2004);
+    printf("Dni od 05.07.2004 do dnia dzisiejszego: %d\n", days);
+    printf("Sekundy od 05.07.2004 do dnia dzisiejszego: %d\n", days*24*3600);
 }
