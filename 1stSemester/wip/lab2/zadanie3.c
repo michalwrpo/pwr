a254134b05d8699bdf9b6721ec81df2d296de822
michalwrpo <michal.p.walus@gmail.com>
2023-11-08 21:19:34 +0100

    minor simplification

--- 1stSemester/wip/lab2/zadanie3.c
+++ 1stSemester/wip/lab2/zadanie3.c
@@ -5,7 +5,7 @@
 
 int main(void) {
     double product = 1;
-    for (int i = 1; i < 501; i++) {
+    for (int i = 1; i <= 500; i++) {
         product *= pow( (i * (1001-i)), 1.0/100);
     }
     printf("%lf\n", pow(product, 1.0/10));
