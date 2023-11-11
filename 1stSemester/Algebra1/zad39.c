76161790bf2f740d09cae2b2003df6ab5c1a809a
michalwrpo <michal.p.walus@gmail.com>
2023-11-11 02:36:13 +0100

    simple gcd algorithm

---
+++ 1stSemester/Algebra1/zad39.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    int temp;
+    if(a < b) {
+        temp = b;
+        b = a;
+        a = temp;
+    }
+    while (b != 0) {
+        temp = a - (a / b * b);
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main(void) {
+    int a, b;
+    printf("Choose value for a: ");
+    scanf("%d", &a);
+    printf("Choose value for b: ");
+    scanf("%i", &b);
+    printf("The greatest common divisor of %d and %d is %d.\n", a, b, gcd(a, b));
+    return 0;
+}
