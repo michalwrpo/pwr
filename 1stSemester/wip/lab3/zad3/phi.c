105d5efb8f9b67183b97a123c037836ee11f942a
michalwrpo <michal.p.walus@gmail.com>
2023-12-04 03:17:48 +0100

    fix: fix  the case of big prime divisors

--- 1stSemester/wip/lab3/zad3/phi.c
+++ 1stSemester/wip/lab3/zad3/phi.c
@@ -6,16 +6,23 @@ int phi(long int n) {
     long int ans = n, k = 3;
     if(n % 2 == 0) {
         ans /= 2;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        
     }
     while(k * k <= n) {
         if(n % k == 0) {
             ans /= k;
             ans *= k-1;
+            while (n % k == 0) {
+                n /= k;
+            }
+            k = 1;
         }
         k += 2;
     }
-    if(ans == n) {
-        ans--;
-    }
+    ans *= n-1;
+    ans /= n;
     return ans;     
 }
