3b3a35cb3dc5ab179ab5d2e628bcd5b6c05c3cc2
michalwrpo <michal.p.walus@gmail.com>
2024-03-20 21:10:22 +0100

    feat: inputs and converter tests

--- 2ndSemester/kursprogramowania/lista1/cpp/app/main.cpp
+++ 2ndSemester/kursprogramowania/lista1/cpp/app/main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 
+#include <valueConverter.hpp>
+#include <primeNumbers.hpp>
+
+
 int main(const int argc, const char* const argv[])
 {
     if (argc < 2)
     {
         std::cerr << "Usage: " << argv[0] << " <arg1> <arg2> ..." << std::endl;
         return 1; 
+    }
+
+    PrimeNumbers* p;
+
+    try
+    {
+        const int plenght = ValueConverter::stringToVal<int>(std::string(argv[1]));
+        p = new PrimeNumbers(plenght);
+    }
+    catch(const std::invalid_argument& e)
+    {
+        std::cerr << "Invalid Argument Error: " << e.what() << std::endl;
+        return 2;
+    }
+    
+
+    for (size_t i = 2; i < static_cast<size_t>(argc); i++)
+    {
+        try
+        {
+            const int argNumber = ValueConverter::stringToVal<int>(std::string(argv[i]));
+            const int prime = p->mthprime(argNumber);
+
+            std::cout << std::to_string(argNumber) << " -> " << std::to_string(prime) << std::endl;
+        }
+        catch(const std::invalid_argument& e)
+        {
+            std::cerr << argv[i] << " -> Invalid Argument Error: " << e.what() << std::endl;
+        }
+        catch(const std::out_of_range& e)
+        {
+            std::cerr << argv[i] << " -> Out of Range Error: " << e.what() << std::endl;
+        }
+        
     }
     
     return 0;
--- 2ndSemester/kursprogramowania/lista1/cpp/test/test.cpp
+++ 2ndSemester/kursprogramowania/lista1/cpp/test/test.cpp
@@ -1,9 +1,11 @@
 extern void testPrimes() noexcept(true);
 extern void testPrimeNumbers() noexcept(true);
+extern void testValueConverter() noexcept(true);
 
 int main(void)
 {
     testPrimes();
     testPrimeNumbers();
+    testValueConverter();
     return 0;
 }
