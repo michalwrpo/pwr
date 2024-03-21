42aae1af96f95e7359039540f41c76fad7859f43
michalwrpo <michal.p.walus@gmail.com>
2024-03-21 01:33:28 +0100

    fix: typo

--- 2ndSemester/kursprogramowania/lista1/cpp/src/primeNumbers.cpp
+++ 2ndSemester/kursprogramowania/lista1/cpp/src/primeNumbers.cpp
@@ -15,10 +15,10 @@ int PrimeNumbers::mthprime(const int m) noexcept(false)
     if (m < 0)
         throw std::invalid_argument(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "parameter n should be nonnegative, got " + std::to_string(m));
 
-    int plenght = static_cast<int>(primes.size());
+    int plength = static_cast<int>(primes.size());
 
-    if (m > plenght - 1)
-        throw std::out_of_range(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "parameter m should be smaller than length of the primes vector (" + std::to_string(plenght) + "), got " + std::to_string(m));
+    if (m > plength - 1)
+        throw std::out_of_range(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "parameter m should be smaller than length of the primes vector (" + std::to_string(plength) + "), got " + std::to_string(m));
 
     return primes[static_cast<size_t>(m)];
 }
