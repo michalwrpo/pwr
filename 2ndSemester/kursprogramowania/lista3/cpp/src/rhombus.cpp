73287d1e8eb76dca97bad2cedb17cec05cbcd786
michalwrpo <michal.p.walus@gmail.com>
2024-04-10 21:41:11 +0200

    fix: typo

--- 2ndSemester/kursprogramowania/lista3/cpp/src/rhombus.cpp
+++ 2ndSemester/kursprogramowania/lista3/cpp/src/rhombus.cpp
@@ -9,7 +9,7 @@ Rhombus::Rhombus(double side, double angle) noexcept(false)
         throw std::invalid_argument(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "side length should be positive, got " + std::to_string(side));
 
     if (angle <= 0 || angle >= 180)
-        throw std::invalid_argument(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "angle should be between 0 adn 180 degrees, got " + std::to_string(angle));
+        throw std::invalid_argument(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " [" + __func__ + "] " + "angle should be between 0 and 180 degrees, got " + std::to_string(angle));
 
     this->side = side;
     this->angle = angle;
