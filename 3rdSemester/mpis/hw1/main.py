3a472257e09ceff48ae9b7ff456eb685f4def77a
michalwrpo <michal.p.walus@gmail.com>
2024-10-27 21:45:33 +0100

    mpis homework 1

---
+++ 3rdSemester/mpis/hw1/main.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Callable, List
+
+def f1(x: float) -> float:
+    return x**(1/3)
+
+def f2(x: float) -> float:
+    return np.sin(x)
+
+def f3(x: float) -> float:
+    return 4*x*(1-x)**3
+
+def makegraph(fileName: str, x: List[int], y: List[float], avgX: List[int], avgY: List[float], trueValue: float, title: str, subtitle: str) -> None:
+    plt.clf()
+    plt.grid(True)
+    plt.scatter(x, y, c='b', s=1, label="Approximated value")
+    plt.scatter(avgX, avgY, c='r', s=8, label="Mean approximated value")
+    plt.axhline(trueValue, c='g', label="True value")
+    plt.xlabel('Number of points')
+    plt.ylabel('Approximated value')
+    plt.suptitle(title)
+    plt.title(subtitle)
+    plt.legend()
+    plt.savefig(fileName)
+
+def approximate(f: Callable, n: int, a: float, b: float, M: float) -> float:
+    if not callable(f):
+        raise TypeError("Function should be callable")
+    if (a > b):
+        raise ValueError("Beginning of an interval should be smaller than its end")
+    
+    C = 0
+    for i in range(n):
+        if M * np.random.random_sample() < f(a + (b - a) * np.random.random_sample()):
+            C += 1
+    return C/n * M * (b - a)
+
+def simulate(f: Callable, a: float, b: float, M: float, Nmin: int, Nmax: int, Nstep: int, K: int, trueValue: float, plotTitle: str, plotSubtitle: str, fileName: str) -> None:
+    if not callable(f):
+        raise TypeError("Function should be callable")
+    if (Nmax - Nmin) % Nstep != 0:
+        raise ValueError("Range of number of tests should be divisible by step")
+    if (a > b):
+        raise ValueError("Beginning of an interval should be smaller than its end")
+    
+    resultsX = []
+    resultsY = []
+    avgX = []
+    avgY = []
+    for n in range(Nmin, Nmax + Nstep, Nstep):
+        sumY = 0
+        for i in range(K):
+            print(f'Simulating {f.__name__} for n={n}, test {i+1}... ', end='')
+            y = approximate(f, n, a, b, M)
+            resultsX.append(n)
+            resultsY.append(y)
+            sumY += y
+            print(f'Result: {y:10.5f}')
+        avgX.append(n)
+        avgY.append(sumY/K)
+
+    makegraph(fileName, resultsX, resultsY, avgX, avgY, trueValue, plotTitle, plotSubtitle)
+
+    return None
+
+def quickSimulate(f: Callable, a: float, b: float, M: float, K: int, trueValue: float, fName: str) -> None:
+    simulate(f, a, b, M, 50, 5000, 50, K, trueValue, f"Approximation of {fName}", "$n_{min} = 50, n_{max} = 5000, n_{step} = 50, K = $" + f"${K}$", f.__name__ + "k" + str(len(str(K))) + ".png")
+
+
+if __name__ == "__main__":
+    quickSimulate(f1, 0, 8, 4, 5, 12, "$\sqrt[3]{x}$")
+    quickSimulate(f1, 0, 8, 4, 50, 12, "$\sqrt[3]{x}$")
+    quickSimulate(f2, 0, np.pi, 1.5, 5, 2, "$\sin{x}$")
+    quickSimulate(f2, 0, np.pi, 1.5, 50, 2, "$\sin{x}$")
+    quickSimulate(f3, 0, 1, 0.5, 5, 0.2, "$4x(1-x)^3$")
+    quickSimulate(f3, 0, 1, 0.5, 50, 0.2, "$4x(1-x)^3$")
---
+++ 3rdSemester/mpis/hw1/mpis_hw1.md
@@ -0,0 +1,12 @@
+# MPiS - Zadanie Domowe 1
+## Michał Waluś
+
+### Zadanie 1
+
+#### a)
+
+- $\int_0^8 \sqrt[3]{x} \,dx = \int_0^8 x^{\frac{1}{3}} \,dx = \frac{3}{4}x^{\frac{4}{3}} \Big\rvert_{x=0}^8 = \frac{3}{4} \left(8^{\frac{4}{3}} - 0^{\frac{4}{3}} \right) = \frac{3}{4} \cdot 16 = 12$ 
+
+- $\int_0^{\pi} \sin(x) \,dx = -\cos(x) \Big\rvert_{x=0}^{\pi} = -\left( \cos(\pi) - \cos(0) \right) = - \left( -1 - 1 \right) = 2$
+
+- $\int_0^1 4x\left(1-x\right)^3 \,dx = 4 \int_0^1 \left(1-x\right)x^3 \,dx = 4 \int_0^1 \left(x^3 - x^4\right) \,dx = 4 \cdot \left( \frac{1}{4} x^4 - \frac{1}{5} x^5 \right) \Big\rvert_{x=0}^1 = 4 \cdot \left( \frac{1}{4} - \frac{1}{5} \right) = 1 - \frac{4}{5} = \frac{1}{5} = 0.2$
