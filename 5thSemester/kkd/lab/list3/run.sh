ad9435ed6c2b1afa29a3407d0fec3c01468d2bfb
michalwrpo <michal.p.walus@gmail.com>
2025-11-12 12:30:41 +0100

    kkd lab 3 run script

--- 5thSemester/kkd/lab/list3/run.sh
+++ 5thSemester/kkd/lab/list3/run.sh
@@ -1,8 +1,8 @@
 for i in $(seq 1 3);
 do
-    for j in $(seq 0 3):
+    for j in $(seq 0 3);
     do
-        echo "test${i}.bin algorithm: ${j}"
+        echo -e "\ntest${i}.bin algorithm: ${j}"
         ./a.out ../list2/test/test${i}.bin coded${i}_${j}.out ${j}
         ./a.out coded${i}_${j}.out decoded${i}_${j}.out ${j} -d
     done
