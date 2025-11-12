5d29483f456e3dca9c13d311fa6c11119507efcb
michalwrpo <michal.p.walus@gmail.com>
2025-11-12 11:44:52 +0100

    kkd lab 3 run script

---
+++ 5thSemester/kkd/lab/list3/run.sh
@@ -0,0 +1,9 @@
+for i in $(seq 1 3);
+do
+    for j in $(seq 0 3):
+    do
+        echo "test${i}.bin algorithm: ${j}"
+        ./a.out ../list2/test/test${i}.bin coded${i}_${j}.out ${j}
+        ./a.out coded${i}_${j}.out decoded${i}_${j}.out ${j} -d
+    done
+done 
