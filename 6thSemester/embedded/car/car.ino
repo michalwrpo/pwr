f6b83bb66dc90312535f75f57619a56d05bb56f5
michalwrpo <michal.p.walus@gmail.com>
2026-05-01 00:48:50 +0200

    embedded

--- 6thSemester/embedded/car/car.ino
+++ 6thSemester/embedded/car/car.ino
@@ -1,6 +1,9 @@
 #include "Wheels.h"
+#include "ir_remote_codes.h"
+
 #include "LiquidCrystal_I2C.h"
 
+#include <IRremote.h>
 #include <Servo.h>
 
 #define LCDAddress 0x27
@@ -10,6 +13,7 @@
 #define TRIG A1
 #define ECHO A2
 
+#define IR_PIN 2
 #define SERVO 9
 
 LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
@@ -21,19 +25,21 @@ Wheels w;
 long argIn = 0;
 long dist = 100000; // in mm
 long beep_delay = 250; // in milliseconds
-long look_delay = 400;
+long look_delay = 500;
+long lcd_delay = 100;
 unsigned long t_move_start = 0;
 unsigned long t_lcd = 0;
 unsigned long t_beep = 0;
-unsigned long t_lookr = 0;
-unsigned long t_look = 200;
-unsigned long t_lookl = 400;
-int lcd_delay = 100;
+unsigned long t_lookl = 0;
+unsigned long t_look = look_delay / 2;
+unsigned long t_lookr = look_delay;
 bool blocked = false;
 
 long angle = 90;
 long distance = 0;
 long dr = 0, dc = 0, dl = 0;
+
+enum IRRemoteCode ir_code = 0;
 
 
 void setup() {
@@ -48,95 +54,109 @@ void setup() {
     pinMode(BEEPER, OUTPUT);
     
     Serial.begin(9600);
-    Serial.setTimeout(200);
+    // Serial.setTimeout(200);
 
     lcd.init();
     lcd.backlight();
 
-    checkInFront();
+    IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 
     w.setSpeed(200);
-    w.forward();
 }
 
 void loop() {
-    unsigned long t = millis();
-    
-    argIn = Serial.parseInt(SKIP_ALL);
-    
-    if (argIn > 0) {
-        dist = argIn * 10;
-        t_move_start = t;
-        w.setSpeed(200);
-        w.forward();
-    } else if (argIn < 0) {
-        dist = argIn * (-10);
-        t_move_start = t;
-        w.setSpeed(200);
-        w.back();
+    if (IrReceiver.decode()) {
+        int new_ir_code = IrReceiver.decodedIRData.command;
+        if (new_ir_code) ir_code = new_ir_code;
+        Serial.println(ir_code);
+        IrReceiver.resume();
+    } else {
+        // ir_code = 0;
     }
 
-    long new_dist = 0;
+    // Serial.println(ir_code == IR1);
+    switch (ir_code) {
+    case IR1:
+    {
+        if (dist > 0) w.forward();
+        unsigned long t = millis();
+        
+        // argIn = Serial.parseInt(SKIP_ALL);
+        
+        // if (argIn > 0) {
+        //     dist = argIn * 10;
+        //     t_move_start = t;
+        //     w.setSpeed(200);
+        //     w.forward();
+        // } else if (argIn < 0) {
+        //     dist = argIn * (-10);
+        //     t_move_start = t;
+        //     w.setSpeed(200);
+        //     w.back();
+        // }
 
-    if (dist > 0) {
-        new_dist = dist - (long)(t - t_move_start) * MMPS / 1000;
-        dist = (new_dist <= 0) ? 0 : dist;
+        long new_dist = 0;
+
+        if (dist > 0) {
+            new_dist = dist - (long)(t - t_move_start) * MMPS / 1000;
+            dist = (new_dist <= 0) ? 0 : dist;
+        }
+
+        if (dist && (dr < 30 || dc < 30 || dl < 30)) {
+            blocked = true;
+            dodge();
+            blocked = false;
+        }
+
+
+        if (t >= t_lcd + lcd_delay) {
+            t_lcd = t;
+            lcdDisplay();
+        }
+
+        // if (t >= t_beep + beep_delay) {
+        //     t_beep = t;
+        //     doBeep();
+        // }
+
+        if (!blocked && t >= t_lookl + 2 * look_delay) {
+            t_lookl += 2 * look_delay;
+            angle = 120;
+            lookAndTellDistance();
+            dl = distance;
+        }
+
+        if (!blocked && t >= t_look + look_delay) {
+            t_look += look_delay;
+            angle = 90;
+            lookAndTellDistance();
+            dc = distance;
+        }
+
+        if (!blocked && t >= t_lookr + 2 * look_delay) {
+            t_lookr += 2 * look_delay;
+            angle = 60;
+            lookAndTellDistance();
+            dr = distance;
+        }
+        
+
+        Serial.print(t_lookl);
+        Serial.print(t_look);
+        Serial.print(t_lookr);
+        Serial.println("");
+
+        if (dist == 0) w.stop();
+
+        break;
     }
-
-    if (dist && (dr < 30 || dc < 30 || dl < 30)) {
-        blocked = true;
-        dodge();
-        blocked = false;
-    }
-
-
-    if (t >= t_lcd + lcd_delay) {
-        t_lcd += lcd_delay;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print((new_dist > 0) ? new_dist : 0);
-        lcd.setCursor(6, 0);
-        lcd.print(angle);
-        lcd.setCursor(10, 0);
-        lcd.print(dl);
-        lcd.setCursor(12, 0);
-        lcd.print(dc);
-        lcd.setCursor(14, 0);
-        lcd.print(dr);
-        lcd.setCursor(0, 1);
-        lcd.print(w.get_lspeed());
-        lcd.setCursor(12, 1);
-        lcd.print(w.get_rspeed());
-    }
-
-    if (t >= t_beep + beep_delay) {
-        t_beep += beep_delay;
-        doBeep();
-    }
-
-    if (!blocked && t >= t_look + look_delay) {
-        t_look = t;
-        angle = 90;
-        lookAndTellDistance();
-        dc = distance;
-    }
-
-    if (!blocked && t >= t_lookr + 2 * look_delay) {
-        t_lookr = t;
-        angle = 60;
-        lookAndTellDistance();
-        dr = distance;
-    }
-
-    if (!blocked && t >= t_lookl + 2 * look_delay) {
-        t_lookl = t;
-        angle = 120;
-        lookAndTellDistance();
-        dl = distance;
-    }
-
-    if (dist == 0) {
+    case IR2:
         w.stop();
+        break;
+    
+    default:
+        w.stop();
+        break;
     }
 }
 
@@ -145,6 +165,25 @@ void doBeep() {
     if (w.get_lspeed() < 0 && w.get_rspeed() < 0) {
         digitalWrite(BEEPER, digitalRead(BEEPER) ^ 1);
     }
+}
+
+void lcdDisplay() {
+    t_lcd += lcd_delay;
+    lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.print((new_dist > 0) ? new_dist : 0);
+    lcd.setCursor(6, 0);
+    lcd.print(angle);
+    lcd.setCursor(10, 0);
+    lcd.print(dl);
+    lcd.setCursor(12, 0);
+    lcd.print(dc);
+    lcd.setCursor(14, 0);
+    lcd.print(dr);
+    lcd.setCursor(0, 1);
+    lcd.print(w.get_lspeed());
+    lcd.setCursor(12, 1);
+    lcd.print(w.get_rspeed());
 }
 
 void lookAndTellDistance() {
@@ -160,31 +199,8 @@ void lookAndTellDistance() {
     distance = tot/59;
 }
 
-void checkInFront() {
-    angle = 60;
-    servo.write(angle);
-    lookAndTellDistance();
-    dr = distance;
-    delay(150);
-
-    angle = 90;
-    servo.write(angle);
-    lookAndTellDistance();
-    dc = distance;
-    delay(150);
-
-    angle = 120;
-    servo.write(angle);
-    lookAndTellDistance();
-    dl = distance;
-    delay(150);
-
-    angle = 90;
-    servo.write(angle);
-}
-
 void dodge() {
-    int process_delay = 100;
+    int process_delay = 200;
 
     w.stop();
     angle = 30;
---
+++ 6thSemester/embedded/car/ir_remote_codes.h
@@ -0,0 +1,24 @@
+#ifndef IR_REMOTE_CODES_H
+#define IR_REMOTE_CODES_H
+
+enum IRRemoteCode {
+    IR1 = 0x45,
+    IR2 = 0x46,
+    IR3 = 0x47,
+    IR4 = 0x44,
+    IR5 = 0x40,
+    IR6 = 0x43,
+    IR7 = 0x7,
+    IR8 = 0x15,
+    IR9 = 0x9,
+    IRstar = 0x16,
+    IR0 = 0x19,
+    IRhash = 0xD,
+    IRup = 0x18,
+    IRleft = 0x8,
+    IRok = 0x1C,
+    IRright = 0x5A,
+    IRdown = 0x52,
+};
+
+#endif // IR_REMOTE_CODES_H
